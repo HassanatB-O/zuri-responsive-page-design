fd27cabf691213ed14c57850f66402d804143963
Hassanat Busari <60980025+HassanatB-O@users.noreply.github.com>
2021-04-09 21:17:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-links');
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+});
